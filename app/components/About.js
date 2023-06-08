6995f5f3a4ad9d729fc1e48330c318790343e41c
mdfaisal <mdfaisal2699@gmail.com>
2023-06-08 14:44:07 +0530

    About us & TC

--- app/components/About.js
+++ app/components/About.js
@@ -1,17 +1,29 @@
 import React from "react";
 import Page from "./Page";
 
-function About(){
-  return(
+function About() {
+  return (
     <>
-    <Page title="About Us">
-      <h2>About Us</h2>
-      <p className="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolorum labore quisquam vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid error veritatis consequuntur, excepturi cumque fuga eum incidunt doloremque?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At qui enim rem totam voluptatum. Aut saepe temporibus, facilis ex a iste expedita minima dolorum dicta doloribus libero aliquid, quae maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit beatae eum, est soluta ducimus ratione et impedit sapiente, nihil, atque dignissimos adipisci? Totam atque officia quis voluptates sed veniam?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quisquam possimus tenetur, dicta enim rerum quis, quaerat id nobis provident quo dolorum sapiente temporibus facere non repellendus consequatur cupiditate!</p>
+      <Page wide={true} title="About Us">
+        <div className="container">
+          <h3 className="mt-4">About Us</h3>
+          <h5 className="mt-4">Welcome to MySocialNet</h5>
+          <p>The premier social networking platform designed to connect individuals from around the world. Our mission is to create a space where people can come together, share their experiences, and build meaningful connections.</p>
+          <h5 className="mt-4">Who We Are</h5>
+          <p>At MySocialNet, we believe in the power of community. Our platform is built on the foundation of bringing people closer, fostering inclusivity, and embracing diversity. We understand that every person has a unique story to tell, and we aim to provide a platform that amplifies those voices and experiences.</p>
+          <h5 class="mt-4">What We Offer</h5>
+          <ul class="list-group">
+            <li class="list-group-item">Connect with Friends, family, and colleagues and share updates, photos, and videos.</li>
+            <li class="list-group-item">Discover diverse communities and engage in vibrant discussions centered around your interests, hobbies, or causes.</li>
+            <li class="list-group-item">Stay updated with the latest news, trends, and events happening locally and globally.</li>
+          </ul>
+          <h5 className="mt-4">Join Us Today</h5>
+          <p>We invite you to join the MySocialNet community and experience the power of connection. Sign up for free, create your profile, and start sharing your journey with the world. Whether you're looking to connect with friends, find new communities, or make a difference, MySocialNet is here to support you every step of the way.</p>
+          <p>Connect. Share. Empower. Welcome to MySocialNet.</p>
+        </div>
       </Page>
     </>
-  )
+  );
 }
 
-export default About
+export default About;
--- app/components/Terms.js
+++ app/components/Terms.js
@@ -1,19 +1,40 @@
 import React from "react";
 import Page from "./Page";
 
-function Terms(){
-  return(
+function Terms() {
+  return (
     <>
-    <Page title="Terms and Conditions">
-      <h2>Our Terms &amp; Conditions</h2>
-      <p className="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolorum labore quisquam vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid error veritatis consequuntur, excepturi cumque fuga eum incidunt doloremque?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At qui enim rem totam voluptatum. Aut saepe temporibus, facilis ex a iste expedita minima dolorum dicta doloribus libero aliquid, quae maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit beatae eum, est soluta ducimus ratione et impedit sapiente, nihil, atque dignissimos adipisci? Totam atque officia quis voluptates sed veniam?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quisquam possimus tenetur, dicta enim rerum quis, quaerat id nobis provident quo dolorum sapiente temporibus facere non repellendus consequatur cupiditate!</p>
-      <h3>Details</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae laboriosam distinctio atque sint earum? Temporibus, voluptas aspernatur aliquam nisi sed harum laborum, nemo odio animi officia quisquam. Veniam, natus reprehenderit.</p>
-    </Page>
+      <Page wide={true} title="Terms and Conditions">
+        <div class="container">
+          <h3 class="mt-4">Terms and Conditions</h3>
+          <p>Welcome to MySocialNet! By accessing or using our website, you agree to comply with these terms and conditions.</p>
+
+          <h5>Intellectual Property</h5>
+          <p>All content on MySocialNet is the property of MySocialNet and is protected by intellectual property laws. You may not use, modify, reproduce, distribute, or transmit any content without prior written consent.</p>
+
+          <h5>User Conduct</h5>
+          <p>When using MySocialNet, you agree to provide accurate information, respect the rights of others, and use the website solely for lawful purposes.</p>
+
+          <h5>Privacy</h5>
+          <p>We value your privacy. Please refer to our Privacy Policy for information on how we collect, use, and protect your personal information.</p>
+
+          <h5>Third-Party Websites</h5>
+          <p>MySocialNet may contain links to third-party websites. We do not endorse or assume responsibility for the content or privacy practices of these websites.</p>
+
+          <h5>Limitation of Liability</h5>
+          <p>MySocialNet is provided "as is" without warranties. We shall not be liable for any damages arising from your use of the website.</p>
+
+          <h5>Modifications to Terms and Conditions</h5>
+          <p>We reserve the right to modify these terms and conditions at any time without prior notice.</p>
+
+          <h5>Governing Law</h5>
+          <p>These terms and conditions shall be governed by and construed in accordance with the laws of [Jurisdiction].</p>
+
+          <p>By using MySocialNet, you acknowledge that you have read, understood, and agree to these terms and conditions.</p>
+        </div>
+      </Page>
     </>
-  )
+  );
 }
 
-export default Terms
+export default Terms;
